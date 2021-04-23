440ef34db88ed070d5936694e354788c9434ded6
Dominik <dmosen@users.noreply.github.com>
2021-04-23 12:02:29 +0200

    traceback

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python2
-name = input("Name: ")
-print 'Hello {}!'.format(name)
+import traceback
+
+def do_computation():
+    raise Exception("Secret info")
+
+def __init__():
+    try:
+        do_computation()
+    except Exception as e:
+        return traceback.format_exc()
+
+x = __init__()
+print x
